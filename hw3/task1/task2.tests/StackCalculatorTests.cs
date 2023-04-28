a86a96d3ff605713ffa36e4ba836a458b047fc63
TimaFrolov <tima1911@yandex.ru>
2023-04-28 14:20:05 +0300

    Add more tests for stack calculator in hw 3 task 1

--- hw3/task1/task2.tests/StackCalculatorTests.cs
+++ hw3/task1/task2.tests/StackCalculatorTests.cs
@@ -2,8 +2,6 @@ namespace Hw2Task2.Tests;
 
 public class StackCalculatorTests
 {
-    private ArrayStack<StackElement> arrayStack = new();
-
     [Test]
     public void Evaluate_ListStack_Empty_ThrowsIncorrectStackException()
     {
@@ -25,8 +23,8 @@ public class StackCalculatorTests
     public void Evaluate_ListStack_FiveMinusFive_ResultIsZero()
     {
         ListStack<StackElement> stack = new();
-        stack.Push(new StackElement.Number(0));
-        stack.Push(new StackElement.Number(0));
+        stack.Push(new StackElement.Number(5));
+        stack.Push(new StackElement.Number(5));
         stack.Push(new StackElement.Binop(BinopType.Sub));
         Assert.That(StackCalculator.Evaluate(stack), Is.EqualTo((Fraction)0));
     }
@@ -55,10 +53,41 @@ public class StackCalculatorTests
     [Test]
     public void Evaluate_ArrayStack_NotEnoughElement_ThrowsIncorrectStackException()
     {
-        Fraction expectedResult = (Fraction)5 / 3;
         ArrayStack<StackElement> stack = new();
         stack.Push(new StackElement.Number(3));
         stack.Push(new StackElement.Binop(BinopType.Div));
+        Assert.Throws<IncorrectStackException>(() => StackCalculator.Evaluate(stack));
+    }
+
+    [Test]
+    public void Evaluate_ArrayStack_OneDivideByZero_ThrowsException()
+    {
+        ArrayStack<StackElement> stack = new();
+        stack.Push(new StackElement.Number(3));
+        stack.Push(new StackElement.Number(0));
+        stack.Push(new StackElement.Binop(BinopType.Div));
+        Assert.Throws<DivideByZeroException>(() => StackCalculator.Evaluate((stack)));
+    }
+
+    [Test]
+    public void Evaluate_IncorrectStack_ThrowsIncorrectStackException()
+    {
+        ArrayStack<StackElement> stack = new();
+        stack.Push(new StackElement.Binop(BinopType.Div));
+        stack.Push(new StackElement.Binop(BinopType.Mul));
+        stack.Push(new StackElement.Binop(BinopType.Add));
+        stack.Push(new StackElement.Number(10));
+        Assert.Throws<IncorrectStackException>(() => StackCalculator.Evaluate((stack)));
+    }
+
+    [Test]
+    public void Evaluate_ArrayStack_TooManylement_ThrowsIncorrectStackException()
+    {
+        ArrayStack<StackElement> stack = new();
+        stack.Push(new StackElement.Number(3));
+        stack.Push(new StackElement.Binop(BinopType.Div));
+        stack.Push(new StackElement.Number(3));
+        stack.Push(new StackElement.Number(3));
         Assert.Throws<IncorrectStackException>(() => StackCalculator.Evaluate(stack));
     }
 }
