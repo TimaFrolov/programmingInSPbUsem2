843001b0ec9a88967959d03d64f38a8944419234
TimaFrolov <tima1911@yandex.ru>
2023-05-19 15:32:38 +0300

    Add test for hw 6 task 1

--- hw6/task1/task1.tests/Tests.cs
+++ hw6/task1/task1.tests/Tests.cs
@@ -6,13 +6,15 @@ public class Tests
     public void MapOnListReturnsListWithFunctionAppliedToAllElements()
         => Assert.That(CollectionTransformer.Map(x => x * 2, new List<int> { 1, 2, 3 }), Is.EqualTo(new List<int> { 2, 4, 6 }));
 
-
     [Test]
     public void FilterEvenNumbersReturnsEvenNumbers()
         => Assert.That(CollectionTransformer.Filter(x => x % 2 == 0, new List<int> { 1, 2, 3, 4, 5, 10, 9 }), Is.EqualTo(new List<int> { 2, 4, 10 }));
 
-
     [Test]
     public void FoldListReturnsCorrectValue()
         => Assert.That(CollectionTransformer.Fold((acc, elem) => acc * elem, 1, new List<int> { 1, 2, 3 }), Is.EqualTo(6));
+
+    [Test]
+    public void FoldOnEmptyListReturnsInitValue()
+        => Assert.That(CollectionTransformer.Fold((_, _) => -1, 1, new List<int>()), Is.EqualTo(1));
 }
